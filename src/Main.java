cf0d564671fe8164b3c868a60eeb4153f484add5
Darío Chinea Delgado <dariocd0808@gmail.com>
2022-10-18 11:49:15 +0100

    Feature: Se añadió la funcionalidad del programa
    Se compureba si el número de ventanas abiertas por el usuario siguen vivas y se tiermina el porceso cuadno terminan

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        ProcessBuilder pb = new ProcessBuilder("notepad.exe");
+        Process p = null;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Número de ventanas: ");
+
+        int numeroVentanas = sc.nextInt();
+
+        ArrayList<Process> lista = new ArrayList<Process>();
+        sc.nextLine();
+        for (int i = 0; i < numeroVentanas; i++) {
+            p = pb.start();
+            lista.add(p);
+        }
+        for (Process nuevoP : lista) {
+            while (nuevoP.isAlive()) {
+                System.out.println("El proceso esta alive ...");
+            }
+        }
 
     }
 }
